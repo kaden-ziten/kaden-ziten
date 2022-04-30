d576a5337dddd740e0a66f3e9b7136befa0bba63
kaden-ziten <user.torokuyo@gmail.com>
2022-04-30 23:35:38 +0900

    initial2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kaden-ziten', // Usually your GitHub org/user name.
+  projectName: 'kaden-ziten', // Usually your repo name.
 
   presets: [
     [
